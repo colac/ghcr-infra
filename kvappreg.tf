40d6a4fa8303f95a10c072809611bbd54319ebd1
colac <hugof_lacerda@hotmail.com>
2022-07-03 13:41:19 +0100

    removing appreg creation

--- kvappreg.tf
+++ kvappreg.tf
@@ -20,14 +20,14 @@ resource "azuread_application" "kvappreg" {
 #     tags = var.tags
 # }
 # Save secret in kv
-resource "azurerm_key_vault_secret" "kvclientsecret" {
-    name         = "appReg-${var.kvName}-${var.env[terraform.workspace]}-clientSecret"
-    value        = azuread_application_password.kvappregsecret.value
-    key_vault_id = azurerm_key_vault.infrakv.id
-    # Tags to apply
-    tags = var.tags
-}
+# resource "azurerm_key_vault_secret" "kvclientsecret" {
+#     name         = "appReg-${var.kvName}-${var.env[terraform.workspace]}-clientSecret"
+#     value        = azuread_application_password.kvappregsecret.value
+#     key_vault_id = azurerm_key_vault.infrakv.id
+#     # Tags to apply
+#     tags = var.tags
+# }
 # Display secret generated, only for testing purposes. This value is kept in the KV
-output "sensitive_example_hash" {
-    value = nonsensitive(azuread_application_password.kvappregsecret.value)
-}
+# output "sensitive_example_hash" {
+#     value = nonsensitive(azuread_application_password.kvappregsecret.value)
+# }
